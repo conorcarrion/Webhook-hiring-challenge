7f11fd79d0b6669f5244cead0e7fd002fbd419f9
Conor Quinn <cjq234@gmail.com>
2022-12-03 12:10:49 +0000

    added envs and table config

--- webhookhandler.py
+++ webhookhandler.py
@@ -1,21 +1,34 @@
-from flask import Flask, request, abort
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-import json
-import sqlalchemy
 import datetime
 from dotenv import load_dotenv
+import os
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+load_dotenv()
+
+DATABASE_TYPE = os.getenv('DATABASE_TYPE')
+HOST = os.getenv('HOST')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+DATABASE = os.getenv('DATABASE')
+PORT = os.getenv('PORT')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = (f"{DATABASE_TYPE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
 db = SQLAlchemy(app)
 
 
 
-class Events(db.Model):
+class Event(db.Model):
+    __tablename__='changes'
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.Nvarchar('max'), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    data = db.Column(db.NVARCHAR('max'), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now(datetime.timezone.utc))
+
+    def __init__(self, data):
+      self.data = data
 
     
 @app.route('/')
@@ -27,15 +40,13 @@ def webhook():
   print('post received')
   confirm_json = request.headers['content-type'] == 'application/json'
   confirm_push = request.headers['X-Github-Event'] == 'push'
-  confirm_post = request.method == 'POST'
-  if confirm_json and confirm_push and confirm_post:
-      event = request.json
-      with open('event.json', 'w') as outfile:
-          pass
-
-      with open('event.json', 'a') as outfile:
-        json.dump(event, outfile, indent=4)
-      return event          
+  confirm_main = request.json['ref'] == 'refs/heads/main'
+  if confirm_json and confirm_push and confirm_main:
+      data = request.json
+      event = Event(data)
+      db.session.add(event)
+      db.session.commit()
+      return "Push Data added to Database"         
 
 if __name__ == '__main__':
   app.run(debug=True)
