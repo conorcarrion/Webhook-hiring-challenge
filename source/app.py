ab572ee2d7ca495918538c52a06b6233afbb42fe
Conor Quinn <cjq234@gmail.com>
2022-12-10 17:34:32 +0000

    fixed up app.py

--- source/app.py
+++ source/app.py
@@ -5,34 +5,26 @@ import os
 import json
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 
 
-# loading environment variables from .env file
-load_dotenv()
-DATABASE_TYPE = os.getenv("DATABASE_TYPE")
-DBAPI = os.getenv("DBAPI")
-HOST = os.getenv("HOST")
-USER = os.getenv("USER1")
-PASSWORD = os.getenv("PASSWORD")
-DATABASE = os.getenv("DATABASE")
-PORT = os.getenv("PORT")
+# loading environment variables
+user = os.getenv("USER1")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+dbname = os.getenv("DATABASE")
 
-dv_env = f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
-db = SQLAlchemy()
+db_env = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
 
 # instantiating Flask and configuring sqlalchemy database uri based on env variables
-def create_flask_app(db_env):
+def create_flask_app(credentials):
 
     app = Flask(__name__)
-
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_env
+    app.config["SQLALCHEMY_DATABASE_URI"] = credentials
     return app
 
-flask_app = create_flask_app()
-
-db.init_app(flask_app)
+flask_app = create_flask_app(db_env)
 
 # instantiating sqlalchemy database
 db = SQLAlchemy(flask_app)
@@ -40,76 +32,68 @@ db = SQLAlchemy(flask_app)
 
 # Database object to allow insertion of webhook information into sql database
 class ChangeEvent(db.Model):
-
     __tablename__ = "changes"
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.JSON, nullable=False)
 
     def __init__(self, data):
         self.data = data
-
-with flask_app.app_context():
-    db.create_all()
-
-# Converting event to database object and adding/committing to database.
-@classmethod
-class ChangeEventRepository:
-
-    def add_change_event_to_db(db, mod_request_json):
-
-        change_event_object = ChangeEvent(mod_request_json)
-        db.session.add(change_event_object)
-        db.session.commit()
-
-
+     
 # Defining homepage behaviour
 @flask_app.route("/")
 def root():
     return "Welcome to my Github Webhook Handler"
 
-
-# Filtering change events to only json, only main, only branch
-def change_event_filter(request):
-    confirm_json = request.headers["content-type"] == "application/json"
-    confirm_push = request.headers["X-Github-Event"] == "push"
-    confirm_main = request.json["ref"] == "refs/heads/main"
-    return confirm_json and confirm_push and confirm_main
-
-
 # Defining actions to take upon receiving a POST request to url/github
 @flask_app.route("/github", methods=["POST"])
 def webhook_receiver():
+
+    # Filtering change events to only json, only main, only branch
+    def change_event_filter(request):
+        # confirm request format is json 
+        confirm_json = request.headers["content-type"] == "application/json"
+        # confirm webhook trigger is due to a push 
+        confirm_push = request.headers["X-Github-Event"] == "push"
+        # identify the default branch of the repository
+        default_branch = request.json["default_branch"]
+        # confirm the push is for the default branch
+        confirm_default = request.json["ref"].split('/')[-1] == default_branch
+        return confirm_json and confirm_push and confirm_default
+
     # Request meets specification and is actioned
     if change_event_filter(request):
-        change_event_body = request.json
+        request_body = request.json
 
         # information converted to format required
-        mod_request_dict = {
-            "ts": change_event_body["created"],
+        change_event_data = json.dumps{
+            # time of the push event
+            "ts": request_body["head_commit"]["timestamp"],
             "source": "github",
             "change_type": "push",
             "data": {
-                "repository": change_event_body["repository"],
-                "branch": change_event_body["ref"],
-                "commit": change_event_body["after"],
-                "author": change_event_body["pusher"],
-                "message": change_event_body["head_commit"]["message"],
+                # repo name
+                "repository": request_body["repository"]["name"],
+                # name of the branch changes were pushed to
+                "branch": request_body["ref"].split('/')[-1],
+                # commit sha that is the HEAD after the push
+                "commit": request_body["after"],
+                # name/email of the author
+                "author": request_body["head_commit"]["author"][:-1],
+                # last commit message that is not a merge commit
+                "message": request_body["head_commit"]["message"],
             },
         }
-        mod_request_json = json.dumps(mod_request_dict)
-        add_change_event_to_db(db, mod_request_json)
+        
+        db.session.add(ChangeEvent(change_event_data))
+        db.session.commit()
         return "Webhook received and information added to database", 200
 
     # Pushes to repository not on main branch are not actioned
-    if not request.json["ref"] == "refs/heads/main":
-        return "Success, non main branch push received but not added", 200
+    if not request.json["ref"].split('/')[-1] == request.json["default_branch"]:
+        return "Push request not for default branch, not actioned", 200
 
     # POST requests not json or push are flagged as bad requests.
     return 'Bad Request, ', 400
-
-
-# create table as per ChangeEvent or ignore
-db.create_all()
 
 
 if __name__ == "__main__":
